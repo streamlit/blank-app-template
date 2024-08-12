3149b5743bf86fbc28b098497bdd74f07cd45731
PM696 <145720634+PM696@users.noreply.github.com>
2024-08-12 22:46:39 +0000

    app updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,71 @@
 import streamlit as st
 import pickle
+import catboost
 import pandas as pd
-from sklearn import datasets
-from catboost import CatBoostClassifier
-
-clf = pickle.load('best_pipeline.pkl')
+import numpy as np
+import sklearn
+import matplotlib
+import pycaret
+from pycaret.classification import *
+from pycaret.classification import load_model, predict_model
+import os
 
 st.title("🎈 College Classification App")
-st.write("This application classifies universities and colleges into two classes: High - meaning those with more than 20 awards issued for every 100 fulltime students enrolled, and Low - those with les than 20 awards.")
+st.divider()
+st.write("This application classifies universities and colleges.")
 
-st.sidebar.header('User Input Parameters')
+def load_data(x):
+    data = pd.read_csv('ml_dataset.csv')
+    return data
 
+# Load the model
+model = load_model('my_first_best_pipeline_cboost')
 
-def user_input_features():
-    f1 = st.sidebar.slider('f1', 1.0, 70.0)
-    f2 = st.sidebar.slider('f2', 1.0, 20.0)
-    data = {
-        'f1': f1,
-        'f2': f2
-    }
-    features = pd.DataFrame(data, index=[0])
-    return features
+def main():
+    # Load dataset
+    data = load_data('ml_dataset.csv')
 
+    st.subheader("Data")
+    st.write(data.head())
 
-df = user_input_features()
+    # User input Features
+    st.subheader("User Input Features:")
 
-st.subheader('User Input Parameters')
-st.write(df)
+    exp_award_value = st.slider("Funds per award", min_value=float(data['exp_award_value'].min()), max_value=float(data['exp_award_value'].max()))
+    awards_per_state_value = st.slider("Awards given per State", min_value=float(data['awards_per_state_value'].min()), max_value=float(data['awards_per_state_value'].max()))
+    grad_100_value = st.slider("Graduate within time", min_value=float(data['grad_100_value'].min()), max_value=float(data['grad_100_value'].max()))
+    counted_pct = st.slider("Counted percent", min_value=float(data['counted_pct'].min()), max_value=float(data['counted_pct'].max()))
+    ft_pct = st.slider("Percent of fulltime students", min_value=float(data['ft_pct'].min()), max_value=float(data['ft_pct'].max()))
+    pell_value = st.slider("Pell grant amount", min_value=float(data['pell_value'].min()), max_value=float(data['pell_value'].max()))
+    ft_fac_value = st.slider("Fulltime faculty contributions", min_value=float(data['ft_fac_value'].min()), max_value=float(data['ft_fac_value'].max()))
+    grad_150_value = st.slider("Graduate within 1.5 time", min_value=float(data['grad_150_value'].min()), max_value=float(data['grad_150_value'].max()))
+    cohort_size = st.slider("Cohort size", min_value=float(data['cohort_size'].min()), max_value=float(data['cohort_size'].max()))
+    aid_value = st.slider("Aid value", min_value=float(data['aid_value'].min()), max_value=float(data['aid_value'].max()))
 
-college = datasets.load_college()
-X = college.data
-Y = college.target
+    # Prepare the input data
+    input_data = {'Funds per award': exp_award_value,
+                  'Awards given per State': awards_per_state_value,
+                  'Graduate within time': grad_100_value,
+                  'Counted percent': counted_pct,
+                  'Percent of fulltime students': ft_pct,
+                  'Pell grant amount': pell_value,
+                  'Fulltime faculty contributions': ft_fac_value,
+                  'Graduate within 1.5 time': grad_150_value,
+                  'Cohort size': cohort_size,
+                  'Aid value': aid_value}
 
-clf = CatBoostClassifier()
-clf.fit(X, Y)
+    input_df = pd.DataFrame([input_data])
 
-prediction = clf.predict(df)
-prediciton_proba = clf.predict_proba(df)
+    # Make a prediction
+    if st.button("Classify"):
+        prediction = model.predict_model(model, data=input_df)
+        st.write(f"Prediction: {prediction['Label'][0]}")
 
-st.subheader('Class labels')
-st.write(college.target_names)
+    st.feedback("stars")
 
-st.subheader('Prediction')
-st.write(college.target_names[prediction])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+prompt = st.chat_input("Your message")
+if prompt:
+    st.write(f"The user has sent: {prompt}") 
+   
+if __name__ == "__main__":
+    main()
