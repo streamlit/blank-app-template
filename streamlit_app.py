2c1fc77538f8f22f0e81dcbf410fcfe905c301c4
PM696 <145720634+PM696@users.noreply.github.com>
2024-08-12 15:10:20 +0000

    Modified text file and app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
+import pickle
+import pandas as pd
+from sklearn import datasets
+from catboost import CatBoostClassifier
+import joblib
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+clf = joblib.load('my_first_best_pipeline_cboost.pkl')
+
+st.title("🎈 College Classification App")
+st.write("This application classifies universities and colleges into two classes: High - meaning those with more than 20 awards issued for every 100 fulltime students enrolled, and Low - those with les than 20 awards.")
+
+st.sidebar.header('User Input Parameters')
+
+
+def user_input_features():
+    f1 = st.sidebar.slider('f1', 1.0, 70.0)
+    f2 = st.sidebar.slider('f2', 1.0, 20.0)
+    data = {
+        'f1': f1,
+        'f2': f2
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+
+df = user_input_features()
+
+st.subheader('User Input Parameters')
+st.write(df)
+
+college = datasets.load_college()
+X = college.data
+Y = college.target
+
+clf = CatBoostClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediciton_proba = clf.predict_proba(df)
+
+st.subheader('Class labels')
+st.write(college.target_names)
+
+st.subheader('Prediction')
+st.write(college.target_names[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
