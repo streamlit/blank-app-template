86459e51bf10f960dcc3062471cb7c765d97a7f3
PM696 <145720634+PM696@users.noreply.github.com>
2024-08-15 17:15:10 +0000

    updated model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,41 @@
 import streamlit as st
-import pickle
-import catboost
 import pandas as pd
-import numpy as np
-import sklearn
-import matplotlib
-import pycaret
-from pycaret.classification import *
 from pycaret.classification import load_model, predict_model
-import os
 
 st.title("🎈 College Classification App")
 st.divider()
 st.write("This application classifies universities and colleges.")
 
-def load_data(x):
+@st.cache_data
+def load_data():
     data = pd.read_csv('ml_dataset.csv')
     return data
 
+
 # Load the model
-model = load_model('my_first_best_pipeline_cboost')
+model = load_model('my_first_best_pipeline')
+
 
 def main():
     # Load dataset
-    data = load_data('ml_dataset.csv')
+    data = load_data()
+    subset_cols=['exp_award_value',
+        'awards_per_state_value',
+        'grad_100_value',
+        'counted_pct',
+        'ft_pct',
+        'pell_value',
+        'ft_fac_value',
+        'grad_150_value',
+        'cohort_size',
+        'aid_value']
+    df=data[subset_cols]
+    
 
     st.subheader("Data")
-    st.write(data.head())
+    st.write(df.head())
 
-    # User input Features
-    st.subheader("User Input Features:")
-
-    exp_award_value = st.slider("Funds per award", min_value=float(data['exp_award_value'].min()), max_value=float(data['exp_award_value'].max()))
-    awards_per_state_value = st.slider("Awards given per State", min_value=float(data['awards_per_state_value'].min()), max_value=float(data['awards_per_state_value'].max()))
-    grad_100_value = st.slider("Graduate within time", min_value=float(data['grad_100_value'].min()), max_value=float(data['grad_100_value'].max()))
-    counted_pct = st.slider("Counted percent", min_value=float(data['counted_pct'].min()), max_value=float(data['counted_pct'].max()))
-    ft_pct = st.slider("Percent of fulltime students", min_value=float(data['ft_pct'].min()), max_value=float(data['ft_pct'].max()))
-    pell_value = st.slider("Pell grant amount", min_value=float(data['pell_value'].min()), max_value=float(data['pell_value'].max()))
-    ft_fac_value = st.slider("Fulltime faculty contributions", min_value=float(data['ft_fac_value'].min()), max_value=float(data['ft_fac_value'].max()))
-    grad_150_value = st.slider("Graduate within 1.5 time", min_value=float(data['grad_150_value'].min()), max_value=float(data['grad_150_value'].max()))
-    cohort_size = st.slider("Cohort size", min_value=float(data['cohort_size'].min()), max_value=float(data['cohort_size'].max()))
-    aid_value = st.slider("Aid value", min_value=float(data['aid_value'].min()), max_value=float(data['aid_value'].max()))
-
-    # Prepare the input data
+     # Prepare the input data
     input_data = {'Funds per award': exp_award_value,
                   'Awards given per State': awards_per_state_value,
                   'Graduate within time': grad_100_value,
@@ -54,18 +47,47 @@ def main():
                   'Cohort size': cohort_size,
                   'Aid value': aid_value}
 
-    input_df = pd.DataFrame([input_data])
+    input_df = pd.DataFrame({input_data})
 
+    # User input Features
+    st.subheader("User Input Features:")
+
+    exp_award_value = st.slider("Funds per award", min_value=float(
+        df['exp_award_value'].min()), max_value=float(df['exp_award_value'].max()))
+    awards_per_state_value = st.slider("Awards given per State", min_value=float(
+        df['awards_per_state_value'].min()), max_value=float(df['awards_per_state_value'].max()))
+    grad_100_value = st.slider("Graduate within time", min_value=float(
+        df['grad_100_value'].min()), max_value=float(df['grad_100_value'].max()))
+    counted_pct = st.slider("Counted percent", min_value=float(
+        df['counted_pct'].min()), max_value=float(df['counted_pct'].max()))
+    ft_pct = st.slider("Percent of fulltime students", min_value=float(
+        df['ft_pct'].min()), max_value=float(df['ft_pct'].max()))
+    pell_value = st.slider("Pell grant amount", min_value=float(
+        df['pell_value'].min()), max_value=float(df['pell_value'].max()))
+    ft_fac_value = st.slider("Fulltime faculty contributions", min_value=float(
+        df['ft_fac_value'].min()), max_value=float(df['ft_fac_value'].max()))
+    grad_150_value = st.slider("Graduate within 1.5 time", min_value=float(
+        df['grad_150_value'].min()), max_value=float(df['grad_150_value'].max()))
+    cohort_size = st.slider("Cohort size", min_value=float(
+        df['cohort_size'].min()), max_value=float(df['cohort_size'].max()))
+    aid_value = st.slider("Aid value", min_value=float(
+        df['aid_value'].min()), max_value=float(df['aid_value'].max()))
+
+    st.write("After selecting the above features using the slider, press on the 'Classify' button below to determine whether it is a high- or low-award institution.")
+    
     # Make a prediction
     if st.button("Classify"):
-        prediction = model.predict_model(model, data=input_df)
+        prediction = predict_model(model, data=df)
         st.write(f"Prediction: {prediction['Label'][0]}")
 
+    st.subheader("Rate this App")
     st.feedback("stars")
 
-prompt = st.chat_input("Your message")
-if prompt:
-    st.write(f"The user has sent: {prompt}") 
-   
+    st.subheader("Message us:")
+    prompt = st.text_input("Your message")
+    if prompt:
+        st.write(f"The user has sent: {prompt}")
+
+
 if __name__ == "__main__":
     main()
