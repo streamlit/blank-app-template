9da9bfae4f14a70cc5d4108306234808e2829067
Khatri Fenil <130593520+FENIL931@users.noreply.github.com>
2025-05-10 11:21:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hello Fenil!"
 )
